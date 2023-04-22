6c8d304c49830fd9b00e386d7757d86240c2a66d
catani-maximiliano <Catanimaximiliano@gmail.com>
2023-04-22 18:50:55 -0300

    entrega

---
+++ src/public/js/cartID.js
@@ -0,0 +1,33 @@
+const url = window.location.href; 
+const parts = url.split('/'); 
+const id = parts[parts.length - 1]; 
+
+window.onload = function() {
+  fetch(`http://localhost:8081/api/carts/${id}`)
+    .then(response => response.json())
+    .then(data => procesarDatos(data.payload))
+    .catch(error => console.error(error));
+};
+
+const cart = document.getElementById("cart");
+const cartel = document.getElementById("cartel");
+
+function procesarDatos(data) {
+  let html = data.products.map( (data) => {
+    let respon =  
+    `<div class="product-info container">
+      <h2>${data.product.title}</h2>
+      <p>price: ${data.product.price}</p>
+      <p>id: ${data.product._id}</p>
+      <p>quantity: ${data.quantity}</p>
+    </div>`;
+    return respon;
+
+  })
+  cart.innerHTML = html.join('');   
+  cartel.innerHTML = `<div class="container">
+  <button class="btn btn-dark"><a class="text-decoration-none text-light" href='/products'>Go to Products</a></button>
+  <button class="btn btn-dark"><a class="text-decoration-none text-light" href='/carts'>Go to Carts</a></button>
+  </div>`;
+}
+
---
+++ src/public/js/productsID.js
@@ -0,0 +1,33 @@
+const url = window.location.href; 
+const parts = url.split('/');
+const id = parts[parts.length - 1]; 
+
+window.onload = function() {
+  fetch(`http://localhost:8081/api/products/${id}`)
+    .then(response => response.json())
+    .then(data => procesarDatos(data.payload))
+    .catch(error => console.error(error));
+};
+
+const products = document.getElementById("products");
+const cartel = document.getElementById("cartel");
+
+function procesarDatos(data) {
+  products.innerHTML =   
+  `<div class="product-info container">
+    <h2>${data.title}</h2>
+    <img src="${data.thumbnail[0]}" alt="img"  width="400" height="350">
+    <p>description: ${data.description}</p>
+    <p>category: ${data.category}</p>
+    <p>code: ${data.code}</p>
+    <p>status: ${data.status}</p>
+    <p>stock: ${data.stock}</p>
+    <p>_id: ${data._id}</p>
+    <p>price: ${data.price}</p>
+    <div class="container">
+          <button class="btn btn-dark"><a class="text-decoration-none text-light" href='/products'>Go to Product</a></button>
+          <button class="btn btn-dark">Add to card</button>
+          </div>
+  </div>`;
+ 
+}
